213299858d056d45cbe67f69c7289da28bd5f31d
Conner Vieira <connervieira@tutanota.com>
2022-01-23 20:10:20 -0500

    Added comments to config.php

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php
-$admin_account = "cvieira";
+$admin_account = "cvieira"; // This setting determines which account can create and delete posts.
 
-$instance_name = "Promptly";
-$instance_tagline = "A quick, no-nonsense blogging system.";
+$instance_name = "Promptly"; // This setting specifies the name of the instance. It can usually be left as "Promptly", but can also be changed to better integrated with your website.
+$instance_tagline = "A quick, no-nonsense blogging system."; // This is the tagline that gets shown on the front page of your blog.
 
-$enabled_integrated_authentication = false;
-$login_page = "../login.php";
-$signup_page = "../signup.php";
-$signout_page = "../signout.php";
+$theme = "normal"; // This setting determines which stylesheet Promptly will use. Currently, this can be set to either "normal" or "dark".
+
+$enabled_integrated_authentication = false; // This setting determines whether or not Promptly's integrated authentication system is enabled or disabled. If you already have an existing authentication system, you should disable the integrated one to prevent any conflicts and security issues.
+$login_page = "../login.php"; // This setting determines the login page for Promptly. If you're using the integrated authentcation system, set this variable to './auth/signin.php'
+$signup_page = "../signup.php"; // This setting determines the signup page for Promptly. If you're using the integrated authentcation system, set this variable to './auth/signup.php'
+$signout_page = "../signout.php"; // This setting determines the signout page for Promptly. If you're using the integrated authentcation system, set this variable to './auth/signout.php'
 
 $max_post_title_length = 200;
 $max_post_body_length = 10000;
