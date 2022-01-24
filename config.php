b2d2699bbfd61d37ab910a6ebb216a315080daf6
Conner Vieira <connervieira@tutanota.com>
2022-01-23 19:53:39 -0500

    Added some missing semicolons

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ $instance_name = "Promptly";
 $instance_tagline = "A quick, no-nonsense blogging system.";
 
 $enabled_integrated_authentication = false;
-$login_page = "../login.php"
-$signup_page = "../signup.php"
-$signout_page = "../signout.php"
+$login_page = "../login.php";
+$signup_page = "../signup.php";
+$signout_page = "../signout.php";
 
 $max_post_title_length = 200;
 $max_post_body_length = 10000;
