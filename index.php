c0d716e2d8674587871c4f8fc7a4e1500a8426f1
Conner Vieira <connervieira@tutanota.com>
2022-01-24 11:20:35 -0500

    Added more in-depth comments to index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include("./config.php");
+include("./config.php"); // Load the Promptly configuration.
 
 // Check to see if the user is signed in.
 session_start();
@@ -8,8 +8,7 @@ if (isset($_SESSION['loggedin'])) {
 }
 
 
-// Load the posts database from disk.
-$post_database = unserialize(file_get_contents('./blogpostdatabase.txt'));
+$post_database = unserialize(file_get_contents('./blogpostdatabase.txt')); // Load the posts database from disk.
 ?>
 
 
@@ -26,7 +25,7 @@ $post_database = unserialize(file_get_contents('./blogpostdatabase.txt'));
             if ($username == $admin_account) { // Only show the "Create Post" option if the user is signed in as an admin.
                 echo "<div class='button'><a href='./draft.php'>Create Post</a></div>";
             }
-            if ($username !== null) {
+            if ($username !== null) { // Only show the "Sign Out" button if the user is signed in.
                 echo "<div class='button'><a href='" . $signout_page . "'>Sign Out</a></div>";
             }
             ?>
@@ -36,18 +35,18 @@ $post_database = unserialize(file_get_contents('./blogpostdatabase.txt'));
         <hr>
         <div class="posts-view">
             <?php
-            if (sizeof($post_database) > 0 ) {
-                foreach (array_reverse($post_database) as $post) {
+            if (sizeof($post_database) > 0 ) { // Check to see if there are actually any posts in the database.
+                foreach (array_reverse($post_database) as $post) { // Iterate through the post database in reverse so that the 
                     echo "<div class='individual-post'>";
                     echo "<h1>" . $post[2] . "</h1>"; // Show the post title.
                     echo "<p class='post-date'>" . $post[5] . "</p>"; // Show the post date text.
                     echo "<p class='post-text'>" . $post[1] . "</p>"; // Show the post body text.
-                    if ($username == $admin_account) {
+                    if ($username == $admin_account) { // Only show the delete button if the user is signed in as the admin account.
                         echo "<a href='deletepost.php?post_to_delete=" . $post[0] . "'>Delete post</a>";
                     }
                     echo "</div>";
                 }
-            } else {
+            } else { // If there are no posts in the database, show the user
                 echo "<p>There are currently no posts!</p>";
             }
             
