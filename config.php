49393573c77deb09adc93f18fec1e9e3a1f4e1ae
Conner Vieira <connervieira@tutanota.com>
2022-01-23 19:52:43 -0500

    Disabled integrated authentication by default

--- config.php
+++ config.php
@@ -4,8 +4,9 @@ $admin_account = "cvieira";
 $instance_name = "Promptly";
 $instance_tagline = "A quick, no-nonsense blogging system.";
 
-$enabled_integrated_authentication = true;
+$enabled_integrated_authentication = false;
 $login_page = "../login.php"
+$signup_page = "../signup.php"
 $signout_page = "../signout.php"
 
 $max_post_title_length = 200;
